147a9faa975cc6d36514871df5332b47ec2ec76f
WOONIVERSE <104566843+WOONJANG@users.noreply.github.com>
2026-03-26 17:46:52 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 window.APP_CONFIG = {
-  API_BASE: 'https://script.google.com/macros/s/AKfycby3Khq2Qzh_kP4yU3Mqt5ETuUVCiO6GDpLE_gBeNaBtLSoZb_fUAzGUUlqTSK81NTGbOA/exec'
+  API_BASE: 'https://script.google.com/macros/s/AKfycbwR51bVnDAnzkCFMJakRP_YRzUKGw4wNtKmuDascvdvWU39UJ4V8gSRD1bWx1QPoZVCbA/exec'
 };
